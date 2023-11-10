03c370b26163f6552372188a31814791c110db67
M1K3DEV23 <mikery2310@outlook.com>
2023-11-10 11:49:24 -0600

    Proyecto de ambientales finalizado

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // Javascript para la navegacinon horizontal de los botones
 
 // Referencias a los botones
-const btnIzquierda = document.querySelector('.izquierda-btn');
-const btnDerecha = document.querySelector('.derecha-btn');
-const tabMenu = document.querySelector('.tab-menu');
+const btnIzquierda = document.querySelector(".izquierda-btn");
+const btnDerecha = document.querySelector(".derecha-btn");
+const tabMenu = document.querySelector(".tab-menu");
 
 // Visibilidad del icono
 const iconoVisible = () => {
@@ -12,60 +12,84 @@ const iconoVisible = () => {
 
   let anchoScrollable = tabMenu.scrollWidth - tabMenu.clientWidth;
 
-  btnIzquierda.style.display = valorScrollIzq > 0 ? 'block' : 'none';
+  btnIzquierda.style.display = valorScrollIzq > 0 ? "block" : "none";
 
-  btnDerecha.style.display = anchoScrollable > valorScrollIzq ? 'block' : 'none';
+  btnDerecha.style.display = anchoScrollable > valorScrollIzq ? "block" : "none";
 };
 
 // Eventos de los botones
 // Scroll hacia la izquierda
-btnDerecha.addEventListener('click', () => {
+btnDerecha.addEventListener("click", () => {
   tabMenu.scrollLeft += 150;
   //   Llamos a nuestro funcion
   setTimeout(() => iconoVisible(), 50);
 });
 
 // Scroll hacia la derecha
-btnIzquierda.addEventListener('click', () => {
+btnIzquierda.addEventListener("click", () => {
   tabMenu.scrollLeft -= 150;
   // Llamos a nuestra funcion para ver si la izquierda es scrollable
   setTimeout(() => iconoVisible(), 50);
 });
 
 window.onload = () => {
-  btnDerecha.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? 'block' : 'none';
-  btnIzquierda.style.display = tabMenu.scrollWidth >= window.innerWidth ? '' : 'none';
+  btnDerecha.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+  btnIzquierda.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
 };
 
 // Para hacer compatible los iconos de navegacion con los dispositivos moviles
 window.onresize = () => {
-  btnDerecha.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? 'block' : 'none';
-  btnIzquierda.style.display = tabMenu.scrollWidth >= window.innerWidth ? '' : 'none';
+  btnDerecha.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+  btnIzquierda.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
 
   let valorScrollIzq = Math.round(tabMenu.scrollLeft);
 
-  btnIzquierda.style.display = valorScrollIzq > 0 ? 'block' : 'none';
+  btnIzquierda.style.display = valorScrollIzq > 0 ? "block" : "none";
 };
 
 // Javascript para la navegacion jalar con el mouse los botones
 
 let activeDrag = false;
 
-tabMenu.addEventListener('mousemove', (drag) => {
+tabMenu.addEventListener("mousemove", (drag) => {
   if (!activeDrag) return;
 
   tabMenu.scrollLeft -= drag.movementX;
   iconoVisible();
   //   Agregamos una propiedad para saber, cuando el usuario este moviendo con el mouse
-  tabMenu.classList.add('dragging');
+  tabMenu.classList.add("dragging");
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener("mouseup", () => {
   activeDrag = false;
   //   Aqui removemos la clase
-  tabMenu.classList.remove('dragging');
+  tabMenu.classList.remove("dragging");
 });
 
-tabMenu.addEventListener('mousedown', () => {
+tabMenu.addEventListener("mousedown", () => {
   activeDrag = true;
+});
+
+// Javascript para controlar el contenido de los tab
+const tabs = document.querySelectorAll(".tab");
+
+const tabBtns = document.querySelectorAll(".tab-btn");
+
+const tabNav = (tabBtnClick) => {
+  tabBtns.forEach((tabBtn) => {
+    tabBtn.classList.remove("active");
+  });
+
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+
+  tabBtns[tabBtnClick].classList.add("active");
+  tabs[tabBtnClick].classList.add("active");
+};
+
+tabBtns.forEach((tabBtn, i) => {
+  tabBtn.addEventListener("click", () => {
+    tabNav(i);
+  });
 });
